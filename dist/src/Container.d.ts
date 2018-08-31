3b2479b9043b9ca89273b85f0e3c1d0e78031a6b
Mathias Lorenzen <mll@ipaper.io>
2018-08-31 16:57:31 +0200

    Revert "removal of warning."
    This reverts commit ebd47987c0da2688c3bb823a632e222fb3f51d6b.

--- dist/src/Container.d.ts
+++ dist/src/Container.d.ts
@@ -16,6 +16,7 @@ export interface IContainer {
 export declare class Container implements IContainer {
     private readonly _typeMappings;
     private readonly _singletonCache;
+    private _hasResolvedBefore;
     constructor();
     whenResolvingType<T>(requestingType: Constructor<T>): {
         useType: (t: Constructor<T>) => IContainerUseResult;
